3086d56b535109391aefa0fc6a97271929a51b13
chwshuang <hs_yx@foxmail.com>
2015-01-12 16:55:36 +0800

    this is a index

--- src/com/git_demo/core/HelloWorld.java
+++ src/com/git_demo/core/HelloWorld.java
@@ -3,7 +3,9 @@ package com.git_demo.core;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i =0;i < 10; i++){
+			System.out.println("this is a:" + i); 
+		}
 		System.out.println("hello world!");
 	}
 
